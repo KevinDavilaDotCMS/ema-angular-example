9bc13147dab8f2067d33eb5b7e518d9f4311f7d5
KevinDavilaDotCMS <kfariid@gmail.com>
2024-06-17 12:38:59 -0500

    Fixed dotcms url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    dotcmsUrl: "dotcms-qa-master.dotcms.site",
+    dotcmsUrl: "https://dotcms-qa-master.dotcms.site",
     authToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGllZjg0NzhjOS0yMWUzLTQyODMtYTI4Ni0yYWY3MDI4MWU4NjgiLCJ4bW9kIjoxNzE1MDMxOTc0MDAwLCJuYmYiOjE3MTUwMzE5NzQsImlzcyI6ImRvdGNtcy1xYS1tYXN0ZXIiLCJsYWJlbCI6InZlcmNlbCIsImV4cCI6MTgwOTU4MzIwMCwiaWF0IjoxNzE1MDMxOTc0LCJqdGkiOiIyOWUyMTEwMC00ODkzLTRiZDItYTc2My03NWNmMTIwZjQzNGIifQ.1k4ylvpBjr5PRKPvDAIawEIX5gSZLWAn-D1paKH9UCc",
     siteId: "true",
 };

398d97805c5489f2fb8bb18c6f3c6d94a09ef9fb
KevinDavilaDotCMS <kfariid@gmail.com>
2024-06-17 12:25:45 -0500

    Ready to deploy

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  dotcmsUrl: 'http://localhost:8080',
+  /**
+   * This auth token will be exposed in the client-side requests,
+   * for security purposes, make sure is read-only.
+   * More info: https://www.dotcms.com/docs/latest/permissions
+   */
+  authToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGljM2NjMDM1My0zZmUwLTQxNzYtYWNlZS1kMThlMjIzYjIyZGMiLCJ4bW9kIjoxNzE4MDU1Mzc5MDAwLCJuYmYiOjE3MTgwNTUzNzksImlzcyI6IjBkODFlMjUyMjAiLCJsYWJlbCI6ImtkIiwiZXhwIjoxODEyNjAzNjAwLCJpYXQiOjE3MTgwNTUzNzksImp0aSI6IjgzNzVkNzJmLTdkOWItNDBmYy1hNzdjLWQzMmNmZWMxYWRjMyJ9.JZ1q4-VX9S5RIY_sP25ZGXwunYSBnk5Y5d49KKIqFxI',
+  siteId: 'true',
+};
